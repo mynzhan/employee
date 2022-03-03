14bd592f33cb2c7dd22a274ab7e4c7bd0fcc14af
mwcyber <mynzhan111@gmail.com>
2022-03-03 15:47:46 +0600

    first commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,11 @@
+
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Films in CineMachine</h1>
+            <h2>Sum in theater</h2>
+            <h2>Films pay +</h2>
+        </div>
+    )
+}
